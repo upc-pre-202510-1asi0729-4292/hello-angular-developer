4e556cf504c35121bfbd1e590fc28d8a0eb61d54
Angel Velasquez <pcisavel@upc.edu.pe>
2025-04-19 16:41:57 -0500

    feat(main): main behavior.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,27 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import {RegisterDeveloperComponent} from './greetings/components/register-developer/register-developer.component';
+import {GreetDeveloperComponent} from './greetings/components/greet-developer/greet-developer.component';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
+  imports: [
+    RegisterDeveloperComponent,
+    GreetDeveloperComponent
+  ],
   styleUrl: './app.component.css'
 })
 export class AppComponent {
   title = 'hello-angular-developer';
+  public firstName: string = '';
+  public lastName: string = '';
+  public onDeveloperRegistered(developer: { firstName: string; lastName: string }) {
+    this.firstName = developer.firstName;
+    this.lastName = developer.lastName;
+  }
+  public onLater(): void {
+    this.firstName = '';
+    this.lastName = '';
+  }
 }
